8e84e98c87b03159d71fd64a381dc4c602c4bb22
Ptoiusrq <vladimir.yarovoy.jr@gmail.com>
2025-07-19 19:29:13 +0300

    added

--- src/main.js
+++
@@ -1,2 +0,0 @@
-// let promptText = prompt("What's your name?");
-// document.querySelector('.user_name').textContent = promptText;
--- src/modal_welcome.js
+++ src/modal_welcome.js
@@ -1,8 +1,12 @@
   window.onload = function() {
     const modal = document.querySelector(".modal")
     const closeBtn = document.querySelector(".close");
+    let input_welcome = document.querySelector("welcome_input");
+    let username = document.querySelector(".user_name")
 
     modal.style.display = "block";
+
+    welcome_input.textContent = username.textContent
 
     closeBtn.onclick = function() {
       modal.style.display = "none";
@@ -14,3 +18,6 @@
       }
     };
   };
+
+  // let promptText = prompt("What's your name?");
+// document.querySelector('.user_name').textContent = promptText;
